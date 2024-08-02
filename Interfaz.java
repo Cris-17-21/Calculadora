8803ed4b0bce72bb08354baf47013e8584a0f5ee
Cristopher Bazan <cris.hbn17@gmail.com>
2024-08-01 21:42:53 -0500

    Configurando ventana

---
+++ Interfaz.java
@@ -0,0 +1,9 @@
+import javax.swing.JFrame;
+
+public class Interfaz extends JFrame{
+    public Interfaz (){
+        setSize(500, 1000);
+        setVisible(true);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+    }
+}
