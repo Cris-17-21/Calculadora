2938acb138276cda218ea34d234abf13cae54fc9
Cristopher Bazan <cris.hbn17@gmail.com>
2024-08-01 21:54:58 -0500

    Acomodando ventana

--- Interfaz.java
+++ Interfaz.java
@@ -1,8 +1,9 @@
-import javax.swing.JFrame;
+import javax.swing.*;
 
 public class Interfaz extends JFrame{
     public Interfaz (){
-        setSize(500, 1000);
+        setSize(500, 700);
+        setLocation(500, 100);
         setVisible(true);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
     }
