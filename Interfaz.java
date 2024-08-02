d75a928ffdf5c1151814f73324da2c9d57281616
Cristopher Bazan <cris.hbn17@gmail.com>
2024-08-01 22:28:31 -0500

    Agregar título

--- Interfaz.java
+++ Interfaz.java
@@ -1,10 +1,21 @@
+import java.awt.Font;
 import javax.swing.*;
 
 public class Interfaz extends JFrame{
     public Interfaz (){
+        super("Calculadora");
         setSize(500, 700);
         setLocation(500, 100);
         setVisible(true);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
+        //Panel
+        JPanel panel = new JPanel();
+        add(panel);
+        panel.setLayout(null);
+        //Titulo de la calculadora
+        JLabel titulo = new JLabel("Calculadora PRO");
+        panel.add(titulo);
+        titulo.setFont(new Font(getName(), 1, 30));
+        titulo.setBounds(130, 10, 260, 30);
     }
 }
