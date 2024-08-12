5fee43b934a3cc7c6dd7fbc3f95fa3517c259657
Cristopher Bazan <cris.hbn17@gmail.com>
2024-08-11 20:28:39 -0500

    Evento numeros

--- Interfaz.java
+++ Interfaz.java
@@ -1,8 +1,15 @@
 import java.awt.Color;
 import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import javax.swing.*;
 
 public class Interfaz extends JFrame{
+    double result = 0;
+    double primerNumero = 0;
+    double segundoNumero = 0;
+    int aux = 1;
+
     public Interfaz (){
         super("Calculadora");
         setSize(420, 700);
@@ -97,6 +104,178 @@ public class Interfaz extends JFrame{
         btn2.setBounds(120, 580, 70, 70);
         btn3.setBounds(320, 580, 70, 70);
         btn20.setBounds(220, 580, 70, 70);
+
         //EVENTOS
+        //Numeros
+        btn4.addActionListener(new ActionListener() {
+            public void actionPerformed (ActionEvent e){
+                if(aux == 1){
+                    txtf.setText("1");
+                    aux = 0;
+                } else {
+                    txtf.setText(txtf.getText() + "1");
+                }
+            }
+        });
+
+        btn5.addActionListener(new ActionListener() {
+            public void actionPerformed (ActionEvent e){
+                if(aux == 1){
+                    txtf.setText("2");
+                    aux = 0;
+                } else {
+                    txtf.setText(txtf.getText() + "2");
+                }
+            }
+        });
+
+        btn6.addActionListener(new ActionListener() {
+            public void actionPerformed (ActionEvent e){
+                if(aux == 1){
+                    txtf.setText("3");
+                    aux = 0;
+                } else {
+                    txtf.setText(txtf.getText() + "3");
+                }
+            }
+        });
+
+        btn8.addActionListener(new ActionListener() {
+            public void actionPerformed (ActionEvent e){
+                if(aux == 1){
+                    txtf.setText("4");
+                    aux = 0;
+                } else {
+                    txtf.setText(txtf.getText() + "4");
+                }
+            }
+        });
+
+        btn9.addActionListener(new ActionListener() {
+            public void actionPerformed (ActionEvent e){
+                if(aux == 1){
+                    txtf.setText("5");
+                    aux = 0;
+                } else {
+                    txtf.setText(txtf.getText() + "5");
+                }
+            }
+        });
+
+        btn10.addActionListener(new ActionListener() {
+            public void actionPerformed (ActionEvent e){
+                if(aux == 1){
+                    txtf.setText("6");
+                    aux = 0;
+                } else {
+                    txtf.setText(txtf.getText() + "6");
+                }
+            }
+        });
+
+        btn12.addActionListener(new ActionListener() {
+            public void actionPerformed (ActionEvent e){
+                if(aux == 1){
+                    txtf.setText("7");
+                    aux = 0;
+                } else {
+                    txtf.setText(txtf.getText() + "7");
+                }
+            }
+        });
+
+        btn13.addActionListener(new ActionListener() {
+            public void actionPerformed (ActionEvent e){
+                if(aux == 1){
+                    txtf.setText("8");
+                    aux = 0;
+                } else {
+                    txtf.setText(txtf.getText() + "8");
+                }
+            }
+        });
+
+        btn14.addActionListener(new ActionListener() {
+            public void actionPerformed (ActionEvent e){
+                if(aux == 1){
+                    txtf.setText("9");
+                    aux = 0;
+                } else {
+                    txtf.setText(txtf.getText() + "9");
+                }
+            }
+        });
+
+        
+        //Operaciones
+        btn7.addActionListener(new ActionListener() {
+            public void actionPerformed (ActionEvent e){
+                txtf.setText(txtf.getText() + "+");
+            }
+        });
+
+        //Potencia falta mejorar lógica xddd
+        /*btn18.addActionListener(new ActionListener() {
+            public void actionPerformed (ActionEvent e){
+                encontrarNumeros(txtf);
+                if (aux == 0){
+                    txtf.setText("!");
+                    primerNumero = result;
+                    if(primerNumero == 0){
+                        txtf.setText(String.valueOf(primerNumero) + "!");
+                        resultado.setText("1.0");
+                    } else {
+                        result = 1;
+                        for (int j = 1; j <= primerNumero; j++){
+                            result = result * j;
+                        }
+                        txtf.setText(String.valueOf(primerNumero) + "!");
+                        resultado.setText(String.valueOf(result));
+                        System.out.println(result);
+                    } 
+                } else {
+                    txtf.setText(txtf.getText() + "!");
+                    if(primerNumero == 0){
+                        resultado.setText("1.0");
+                    } else {
+                        result = 1;
+                        for (int j = 1; j <= primerNumero; j++){
+                            result = result * j;
+                        }
+                        System.out.println(result);
+                        resultado.setText(String.valueOf(result));
+                    }
+                    aux = 0;
+                }
+            }
+        });*/
+
+        btn20.addActionListener(new ActionListener() {
+            public void actionPerformed (ActionEvent e){
+                txtf.setText("");
+                result = 0;
+            }
+        });
+
+        btn3.addActionListener(new ActionListener() {
+            public void actionPerformed (ActionEvent e){
+                encontrarNumeros(txtf);
+            }
+        });
+    }
+
+    public void encontrarNumeros (JTextField txt){
+        String texto = String.valueOf(txt.getText());
+        for (int i = 0; i < texto.length(); i++){
+            if (texto.charAt(i) == '+' || texto.charAt(i) == '-' || texto.charAt(i) == 'x' || texto.charAt(i) == '/' || texto.charAt(i) == '^'){
+                String num1 = texto.substring(0, i);
+                String num2 = texto.substring(i, texto.length());
+                primerNumero = Double.parseDouble(num1);
+                segundoNumero = Double.parseDouble(num2);
+            } else if (texto.charAt(i) == '√' || texto.charAt(i) == '!'){
+                String num1 = texto.substring(0, i);
+                primerNumero = Double.parseDouble(num1);
+            }
+        }
     }
 }
